137d311778169b3072c6ffc5f6cdacc5014030af
Vincent <vg_4_@hotmail.fr>
2019-10-28 11:09:12 -0400

    correction de probleme avec le server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 //Install express server
-import express from "express";
-import * as cors from cors;
+const express = require("express");
 
 const app = express();
-app.use(cors());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/www"));
